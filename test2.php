fe9d3e7a583e48e4cda9462943c8ce894feccd5f
TantanGit <magayjonathan@gmail.com>
2014-06-22 15:34:52 +0800

    added my name

---
+++ test2.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hello World!!"; ##first program
+echo "Here is the first program"; ##btw thanks for adding comments
+echo "Author name: Magay Jonathan M." ##thanks again for comments  
+?>
